baca56e8c8e5c826510ac23dcf6e55face238557
yuanshuhao <128258241+yuan-shuhao@users.noreply.github.com>
2023-12-21 19:04:18 +0800

    Update canbao.js

--- json/canbao.js
+++ json/canbao.js
@@ -1,1 +1,42 @@
-
+{
+  "code": 0,
+  "message": "成功",
+  "data": [
+    {
+      "label": "未参保",
+      "value": "0",
+      "type": "PSN_INSU_STAS",
+      "valiFlag": "1",
+      "version": "2"
+    },
+    {
+      "label": "正常参保",
+      "value": "1",
+      "type": "PSN_INSU_STAS",
+      "valiFlag": "1",
+      "version": "2"
+    },
+    {
+      "label": "正常参保",
+      "value": "2",
+      "type": "PSN_INSU_STAS",
+      "valiFlag": "1",
+      "version": "2"
+    },
+    {
+      "label": "终止参保",
+      "value": "4",
+      "type": "PSN_INSU_STAS",
+      "valiFlag": "1",
+      "version": "2"
+    },
+    {
+      "label": "无效参保",
+      "value": "3",
+      "type": "PSN_INSU_STAS",
+      "valiFlag": "1",
+      "version": "2"
+    }
+  ],
+  "type": "success"
+}
